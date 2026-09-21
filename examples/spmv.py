5ada1d52ca500a3616c08a6e7b7abe871e70e3bb
xywei <wxy0516@gmail.com>
2026-09-21 09:49:20 -0500

    Preserve SpMV source locations with reduce_sum import

--- examples/spmv.py
+++ examples/spmv.py
@@ -61,7 +61,8 @@ import numpy as np
 from lanky import theorem
 from lanky.prelude import Fn, Nat, Real
 
-from loopty import Arr, Fin, Schedule, kernel, program, reduce_sum
+from loopty import Arr, Fin, Schedule, kernel, program
+from loopty import reduce_sum
 
 # {{{ the kernels and the theorem
 
